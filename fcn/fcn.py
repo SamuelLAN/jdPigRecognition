9ceb9ec8f53b5d504187d6feeb8b7f9b6807a242
Yusen Lin <412206186@qq.com>
2017-12-19 15:33:49 +0800

    [RC] fcn cancel batch normalize

--- fcn/fcn.py
+++ fcn/fcn.py
@@ -31,7 +31,7 @@ class FCN(base.NN):
 
     ''' 参数的配置 '''
 
-    BATCH_SIZE = 12  # 迭代的 epoch 次数
+    BATCH_SIZE = 4  # 迭代的 epoch 次数
     EPOCH_TIMES = 100  # 随机梯度下降的 batch 大小
 
     NUM_CHANNEL = 3  # 输入图片为 3 通道，彩色
@@ -50,7 +50,6 @@ class FCN(base.NN):
 
     ''' 类的配置 '''
 
-    USE_BN = True  # 网络里是否使用了 batch normalize
     USE_BN_INPUT = True  # 输入是否使用 batch normalize
 
     SHOW_PROGRESS_FREQUENCY = 2  # 每 SHOW_PROGRESS_FREQUENCY 个 step show 一次进度 progress
@@ -65,7 +64,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv1_1'][0],
             'b': VGG_MODEL['conv1_1'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -73,7 +71,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv1_2'][0],
             'b': VGG_MODEL['conv1_2'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -87,7 +84,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv2_1'][0],
             'b': VGG_MODEL['conv2_1'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -95,7 +91,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv2_2'][0],
             'b': VGG_MODEL['conv2_2'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -109,7 +104,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv3_1'][0],
             'b': VGG_MODEL['conv3_1'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -117,7 +111,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv3_2'][0],
             'b': VGG_MODEL['conv3_2'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -125,7 +118,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv3_3'][0],
             'b': VGG_MODEL['conv3_3'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -139,7 +131,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv4_1'][0],
             'b': VGG_MODEL['conv4_1'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -147,7 +138,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv4_2'][0],
             'b': VGG_MODEL['conv4_2'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -155,7 +145,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv4_3'][0],
             'b': VGG_MODEL['conv4_3'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -169,7 +158,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv5_1'][0],
             'b': VGG_MODEL['conv5_1'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -177,7 +165,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv5_2'][0],
             'b': VGG_MODEL['conv5_2'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -185,7 +172,6 @@ class FCN(base.NN):
             'type': 'conv',
             'W': VGG_MODEL['conv5_3'][0],
             'b': VGG_MODEL['conv5_3'][1],
-            'bn': True,
             'trainable': False,
         },
         {
@@ -199,7 +185,6 @@ class FCN(base.NN):
             'type': 'conv',
             'shape': [VGG_MODEL['conv5_3'][0].shape[3], 4096],
             'k_size': [7, 7],
-            'bn': True,
         },
         {
             'name': 'dropout_6',
@@ -210,7 +195,6 @@ class FCN(base.NN):
             'type': 'conv',
             'shape': [4096, 4096],
             'k_size': [1, 1],
-            'bn': True,
         },
         {
             'name': 'dropout_7',
@@ -221,7 +205,6 @@ class FCN(base.NN):
             'type': 'conv',
             'shape': [4096, NUM_CLASSES],
             'k_size': [1, 1],
-            'bn': True,
             'activate': False,
         },
         {
