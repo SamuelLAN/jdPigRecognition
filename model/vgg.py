f3b5d823951f588d09de2657629aa66eceb9158f
Yusen Lin <412206186@qq.com>
2017-12-19 16:14:45 +0800

    [RC] modify vgg path

--- model/vgg.py
+++ model/vgg.py
@@ -16,6 +16,13 @@ class VGG:
     def __init__(self):
         pass
 
+    @staticmethod
+    def __change_dir():
+        cur_dir = os.path.abspath(os.path.curdir)
+        chang_dir = os.path.abspath(os.path.split(__file__)[0])
+        os.chdir(chang_dir)
+        return cur_dir
+
     ''' 加载模型 '''
 
     @staticmethod
@@ -24,12 +31,19 @@ class VGG:
         Returns:
             vgg_mode (dict)
         '''
+
+        cur_dir = VGG.__change_dir()  # 切回当前目录到文件所在目录
+
         if not os.path.isfile(VGG.MODEL):
             print('Start downloading %s' % VGG.MODEL)
             file_path, _ = urlretrieve(VGG.MODEL_URL, VGG.MODEL, reporthook=VGG.__download_progress)
             stat_info = os.stat(file_path)
             print('\nSuccesfully downloaded %s %d bytes' % (VGG.MODEL_URL, stat_info.st_size))
-        return np.load(VGG.MODEL, encoding='latin1').item()
+        model = np.load(VGG.MODEL, encoding='latin1').item()
+
+        os.chdir(cur_dir)  # 切回原来的目录
+
+        return model
 
     ''' 下载的进度 '''
 
