e21dceb07a9e894f1b888e03071ef5e83bcbfe1c
Yusen Lin <412206186@qq.com>
2017-12-20 16:03:54 +0800

    [WN] add vgg19

--- model/vgg.py
+++ model/vgg.py
@@ -10,8 +10,10 @@ from six.moves.urllib.request import urlretrieve
 
 
 class VGG:
-    MODEL = r'vgg16.npy'
-    MODEL_URL = r'http://www.lin-baobao.com/model/vgg16.npy'
+    MODEL_16 = r'vgg16.npy'
+    MODEL_19 = r'vgg19.npy'
+    MODEL_16_URL = r'http://www.lin-baobao.com/model/vgg16.npy'
+    MODEL_19_URL = r'http://www.lin-baobao.com/model/vgg19.npy'
 
     def __init__(self):
         pass
@@ -26,7 +28,7 @@ class VGG:
     ''' 加载模型 '''
 
     @staticmethod
-    def load():
+    def load(model_19=False):
         '''
         Returns:
             vgg_mode (dict)
@@ -34,12 +36,15 @@ class VGG:
 
         cur_dir = VGG.__change_dir()  # 切回当前目录到文件所在目录
 
-        if not os.path.isfile(VGG.MODEL):
-            print('Start downloading %s' % VGG.MODEL)
-            file_path, _ = urlretrieve(VGG.MODEL_URL, VGG.MODEL, reporthook=VGG.__download_progress)
+        model = VGG.MODEL_16 if not model_19 else VGG.MODEL_19
+        model_url = VGG.MODEL_16_URL if not model_19 else VGG.MODEL_19_URL
+
+        if not os.path.isfile(model):
+            print('Start downloading %s' % model)
+            file_path, _ = urlretrieve(model_url, model, reporthook=VGG.__download_progress)
             stat_info = os.stat(file_path)
-            print('\nSuccesfully downloaded %s %d bytes' % (VGG.MODEL_URL, stat_info.st_size))
-        model = np.load(VGG.MODEL, encoding='latin1').item()
+            print('\nSuccesfully downloaded %s %d bytes' % (model_url, stat_info.st_size))
+        model = np.load(model, encoding='latin1').item()
 
         os.chdir(cur_dir)  # 切回原来的目录
 
@@ -49,6 +54,5 @@ class VGG:
 
     @staticmethod
     def __download_progress(count, block_size, total_size):
-        sys.stdout.write('\r>> Downloading %s %.1f%%' % (os.path.split(VGG.MODEL)[1],
-                                                         float(count * block_size) / float(total_size) * 100.0))
+        sys.stdout.write('\r>> Downloading %.1f%%' % (float(count * block_size) / float(total_size) * 100.0))
         sys.stdout.flush()
